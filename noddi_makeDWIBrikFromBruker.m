e89761dfe4b042c2633dbe56b39acfbf3292c87c
Robert Bussell <rgbussell@gmail.com>
2016-07-09 16:05:28 -0700

    adding code to make multi-shell bvecs/bvals for bruker data

--- noddi_makeDWIBrikFromBruker.m
+++ noddi_makeDWIBrikFromBruker.m
@@ -1,12 +1,18 @@
-addpath('/home/rbussell/matlab/process7T/processing/');
-addpath('/home/rbussell/matlab/afni_matlab/matlab/');
-addpath('/home/rbussell/matlab/NIfTI_20140122');
+function [] = noddi_MakeDWIBrikFromBruker(studyDir, scanNum, procNum, prefix, outBrikDir)
 
-studyDir='/opt/PV5.1/data/rbussell/nmr/GB_GP120_23_1.zJ1';
-scanNum=12;
-procNum=1;
-outBrikDir='/home/rbussell/devel/dti/160119_GB_GP120_NODDI/GP120_23';
-prefix='dti12'
+addpath(getenv('NIFTI_PATH'));
+addpath(getenv('AFNI_MATLAB_PATH'));
+addpath(getenv('PROCESS7T_PATH'));
+
+%addpath('/home/rbussell/matlab/process7T/processing/');
+%addpath('/home/rbussell/matlab/afni_matlab/matlab/');
+%addpath('/home/rbussell/matlab/NIfTI_20140122');
+
+%studyDir='/opt/PV5.1/data/rbussell/nmr/GB_GP120_23_1.zJ1';
+%scanNum=12;
+%procNum=1;
+%outBrikDir='/home/rbussell/devel/dti/160119_GB_GP120_NODDI/GP120_23';
+%prefix='dti12'
 debugMode=1;
 brikType=3
 options.typestring='3DIM_HEAD_FUNC'
