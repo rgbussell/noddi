69ddcf0be38f8b8eea07d43f78c6e454c13e02f9
Robert Bussell <rgbussell@gmail.com>
2016-07-09 13:37:00 -0700

    adding noddi_makeDWIBrikFromBruker.m to make Bruker dwi files

---
+++ noddi_makeDWIBrikFromBruker.m
@@ -0,0 +1,15 @@
+addpath('/home/rbussell/matlab/process7T/processing/');
+addpath('/home/rbussell/matlab/afni_matlab/matlab/');
+addpath('/home/rbussell/matlab/NIfTI_20140122');
+
+studyDir='/opt/PV5.1/data/rbussell/nmr/GB_GP120_23_1.zJ1';
+scanNum=12;
+procNum=1;
+outBrikDir='/home/rbussell/devel/dti/160119_GB_GP120_NODDI/GP120_23';
+prefix='dti12'
+debugMode=1;
+brikType=3
+options.typestring='3DIM_HEAD_FUNC'
+
+pv2Brik2p3( studyDir, scanNum, procNum, ...
+    outBrikDir, prefix, debugMode, brikType, options )
